d037565e346caa98d45281581855dfcbb3f1d446
Mightpedia <byteakp@gmail.com>
2025-07-19 09:43:47 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
+// routes/index.js
 const express = require('express');
 const authRoutes = require('./auth.routes');
 const chatRoutes = require('./chat.routes');
+const conversationRoutes = require('./conversation.routes.js'); // Import conversation routes
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/chat', chatRoutes);
-
+router.use('/conversations', conversationRoutes); // Add this line
 
 module.exports = router;
