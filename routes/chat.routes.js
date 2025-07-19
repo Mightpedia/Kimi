b393502844cfe1e88073c0780fe249ab028793eb
Mightpedia <byteakp@gmail.com>
2025-07-19 09:22:42 +0530

    Update chat.routes.js

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -5,7 +5,6 @@ const { rateLimitMiddleware } = require('../middleware/rateLimit.middleware');
 const { upload } = require('../middleware/upload.middleware');
 
 const router = express.Router();
-
 router.post('/', 
   authMiddleware, 
   rateLimitMiddleware, 
@@ -13,6 +12,7 @@ router.post('/',
   handleChat
 );
 
+// This route provides the list of available AI models to the frontend.
 router.get('/models', authMiddleware, getAvailableModels);
 
 module.exports = router;
